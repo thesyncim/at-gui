bf3afae1ef86530016143721598e1f39630c2d2a
thesyncim <thesyncim@gmail.com>
2014-06-25 16:37:01 +0100

    basic working example

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,17 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('anyTunnel', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'ngResource',
+  'angularMoment',
+  'anyTunnel.filters',
+  'anyTunnel.services',
+  'anyTunnel.directives',
+  'anyTunnel.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'DashboardCtrl'});
+  $routeProvider.when('/nodes/:id', {templateUrl: 'partials/node.html', controller: 'NodeInfoCtrl'});
+  $routeProvider.otherwise({redirectTo: '/dashboard'});
+}]).value("apiaddr", "http://proxy.euroneves.pt:8080");;
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,16 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('anyTunnel.services', []).
+    factory("Nodes", function ($resource, apiaddr) {
+       var node= $resource(apiaddr + '/nodes/:id', {id: '@id'},
+
+            { all: {method: 'get', url: apiaddr + '/nodes/', isArray: true},
+             search: {method: 'get', url: apiaddr + '/nodes/search/:id', isArray: true}});
+
+        return node;
+
+
+
+    }).
+    value('version', '0.1');
